5a52c53bda05990653169e11b8ef3304da9f185c
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-01-16 21:10:56 -0800

    Add ability to create a new thread

--- src/pages/api/threads.js
+++ src/pages/api/threads.js
@@ -1,9 +1,13 @@
 import { ThreadsService } from '../../backend/threadsService'
 
 export const controller = (req, res) => {
-  console.log('________HERE___________')
   if (req.method === 'GET') {
     const threads = ThreadsService.getAllThreads()
     res.status(200).json(threads)
+  }
+
+  if (req.method === 'POST') {
+    const newThread = ThreadsService.addThread(JSON.parse(req.body))
+    res.status(201).json(newThread)
   }
 }
