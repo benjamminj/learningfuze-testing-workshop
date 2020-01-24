09c5d77d1578b4985b305740c382ff404ae846dc
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-01-23 17:05:30 -0800

    add map function

---
+++ src/utils/map.js
@@ -0,0 +1,21 @@
+/**
+ * Replicates the functionality of Array.prototype.map, which uses a callback
+ * function to transform the array into something else.
+ *
+ * Example usage
+ * ```js
+ * map([1, 2, 3, 4], num => num * 2)
+ *
+ * // returns [2, 4, 6, 8]
+ * ```
+ */
+export const map = (array, callback) => {
+  const newArray = []
+
+  for (const i = 0; i < array.length; i++) {
+    const mappedItem = callback(array[i], i, array)
+    newArray.push(mappedItem)
+  }
+
+  return newArray
+}
